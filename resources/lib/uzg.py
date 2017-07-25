a999d32ded007cecbc49e2d0d98476c2b4bd7bc1
Wouter de Vries <wouter@wbdv.nl>
2017-07-25 19:35:25 +0100

    start on fix for more episodes

--- resources/lib/uzg.py
+++ resources/lib/uzg.py
@@ -12,6 +12,56 @@
 '''
 import urllib2 ,re ,time ,json
 from datetime import datetime
+from HTMLParser import HTMLParser
+
+# create a subclass and override the handler methods
+class MyHTMLParser(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.episodes = []
+        self._current_episode = None
+        self.expect_title = False
+
+    def handle_starttag(self, tag, attrs):
+        classattr = next(iter([x[1] for x in attrs if x[0] == "class"]), None)
+        if tag == "div" and classattr is not None and "npo-asset-tile-container" in classattr:
+            self.depthcount = 0
+            self._current_episode = {
+                'label': "",
+                'date': None,
+            }
+        if tag == "a":
+            hrefattr = next(iter([x[1] for x in attrs if x[0] == "href"]), None)
+            if hrefattr is not None:
+                match = re.search("([0-9]{1,2}-[0-9]{1,2}-[0-9]{4})", hrefattr)
+                if match:
+                    self._current_episode['date'] = datetime.strptime(match.group(1), "%d-%m-%Y").strftime('%Y-%m-%dT%H:%M:%S')
+                    self._current_episode['TimeStamp'] = self._current_episode['date']
+                match = re.search("/([A-Z]+_[0-9]+)$", hrefattr)
+                if match:
+                    self._current_episode['whatson_id'] = match.group(1)
+        if tag == "h2":
+            self.expect_title = True
+        if tag == "img":
+            srcattr = next(iter([x[1] for x in attrs if x[0] == "src"]), None)
+            if srcattr is not None:
+                self._current_episode['thumbnail'] = srcattr
+        if tag == "div" and self.depthcount is not None:
+            self.depthcount += 1
+
+    def handle_endtag(self, tag):
+        if tag == "h2":
+            self.expect_title = False
+        if tag == "div" and self.depthcount is not None:
+            self.depthcount -= 1
+        if self.depthcount == 0:
+            self.episodes.append(self._current_episode)
+            self._current_episode = None
+            self.depthcount = None
+
+    def handle_data(self, data):
+        if self.expect_title:
+            self._current_episode['label'] += data
 
 class Uzg:
         #
@@ -33,29 +83,40 @@ class Uzg:
                 uzgitem = { 'label': serie['name'], 'nebo_id': serie['nebo_id'], 'thumbnail': serie['image'] }
                 uzgitemlist.append(uzgitem)                
             self.overzichtcache = sorted(uzgitemlist, key=lambda x: x['label'], reverse=False)
-            
+
         def __items(self, nebo_id):
-            req = urllib2.Request('http://apps-api.uitzendinggemist.nl/series/'+nebo_id+'.json')
+            req = urllib2.Request("https://www.npo.nl/media/series/{}/episodes?page=1&tilemapping=dedicated&tiletype=asset".format(nebo_id))
             req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0')
+            req.add_header('X-Requested-With', 'XMLHttpRequest')
             response = urllib2.urlopen(req)
-            link=response.read()
+            data = response.read()
             response.close()
-            json_data = json.loads(link)
-            uzgitemlist = list()
-            for aflevering in json_data['episodes']:
-                urlcover = ''
-                if not aflevering['stills']:
-                    urlcover = ''
-                else:
-                    urlcover = aflevering['stills'][0]['url']
-                uzgitem = { 'label': aflevering['name']
-                            , 'date': self.__stringnaardatumnaarstring(datetime.fromtimestamp(int(aflevering['broadcasted_at'])).strftime('%Y-%m-%dT%H:%M:%S'))
-                            , 'TimeStamp': datetime.fromtimestamp(int(aflevering['broadcasted_at'])).strftime('%Y-%m-%dT%H:%M:%S')
-                            , 'thumbnail': urlcover
-                            , 'serienaam': json_data['name']
-                            , 'whatson_id': aflevering['whatson_id']}
-                uzgitemlist.append(uzgitem)
-            self.items = uzgitemlist
+            html = json.loads(data)['tiles']
+            parser = MyHTMLParser()
+            parser.feed(html)
+            self.items = parser.episodes
+        # def __items(self, nebo_id):
+        #     req = urllib2.Request('http://apps-api.uitzendinggemist.nl/series/'+nebo_id+'.json')
+        #     req.add_header('User-Agent', 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:25.0) Gecko/20100101 Firefox/25.0')
+        #     response = urllib2.urlopen(req)
+        #     link=response.read()
+        #     response.close()
+        #     json_data = json.loads(link)
+        #     uzgitemlist = list()
+        #     for aflevering in json_data['episodes']:
+        #         urlcover = ''
+        #         if not aflevering['stills']:
+        #             urlcover = ''
+        #         else:
+        #             urlcover = aflevering['stills'][0]['url']
+        #         uzgitem = { 'label': aflevering['name']
+        #                     , 'date': self.__stringnaardatumnaarstring(datetime.fromtimestamp(int(aflevering['broadcasted_at'])).strftime('%Y-%m-%dT%H:%M:%S'))
+        #                     , 'TimeStamp': datetime.fromtimestamp(int(aflevering['broadcasted_at'])).strftime('%Y-%m-%dT%H:%M:%S')
+        #                     , 'thumbnail': urlcover
+        #                     , 'serienaam': json_data['name']
+        #                     , 'whatson_id': aflevering['whatson_id']}
+        #         uzgitemlist.append(uzgitem)
+        #     self.items = uzgitemlist
 
         def __get_data_from_url(self, url):
             req = urllib2.Request(url)
